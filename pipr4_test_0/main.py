a66ab4163b2545f660c9951d73e78d5ce9e46945
Gustaw Daczkowski <gucio.lektor@gmail.com>
2019-11-11 22:01:19 +0100

    Remove unused directive.

--- pipr4_test_0/main.py
+++ pipr4_test_0/main.py
@@ -1,12 +1,9 @@
-from pipr4_test_0.party import Party
 from pipr4_test_0.voting import Voting
 
-
-
-tickets = 301
+tickets = 300
 threshold = 0.05  # 5% threshold
 
-parties =  {"Flowers":1256, "Cars":4325, "Knights":446, "Queens":100,"Eagles":9880}
+parties = {"Flowers": 1256, "Cars": 4325, "Knights": 446, "Queens": 100, "Eagles": 9880}
 v = Voting(tickets, parties, threshold)
 
 v.print_results()
